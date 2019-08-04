79965ad40a6d759bafc677b1462ae35d67ec2b6f
Elena Zimina <elena.zimina@genestack.com>
2019-08-04 15:50:34 +0300

    Clean up sieve of eratosthenes

--- sieve-of-eratosthenes/index.js
+++ sieve-of-eratosthenes/index.js
@@ -1,7 +1,11 @@
+/**
+ * Returns an array of all the prime numbers which are less than parameter 'n'
+ */
 function getPrimes(n) {
     const arr = new Array(n + 1).fill(true);
     arr[0] = arr[1] = false;
 
+    /** Fill the values with not-prime indexes with 'false', so only prime indexes are staying 'true' */
     for (let i = 2; i < Math.sqrt(arr.length - 1); i++) {
         if (arr[i]) {
             for (let j = Math.pow(i, 2); j < n + 1; j = j + i) {
@@ -10,15 +14,5 @@ function getPrimes(n) {
         }
     }
 
-    return arr;
+    return arr.map((i, index) => i && index).filter((j) => j);
 }
-
-console.log(getPrimes(36).reduce((acc, i, index) => {
-    if (i) {
-        acc.push(index);
-    }
-    return acc;
-}, []));
-
-
-
